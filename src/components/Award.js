0e8ec5a4a04797b3b19ab18bcede4e0b57901512
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-07-31 11:42:21 +0100

    Added an award component and styled Movies detail

---
+++ src/components/Award.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// Styles
+import styled from 'styled-components';
+import { Line } from '../styles/styles';
+
+const Award = ({title, description}) => {
+
+  return (
+    <StyledAward>
+      <h3 className="h4">{title}</h3>
+      <AwardLine></AwardLine>
+      <p className="parag">{description}</p>
+    </StyledAward>
+  );
+};
+
+const AwardLine = styled(Line)`
+  background-color: #23d997;
+`;
+
+const StyledAward = styled.div`
+  flex-basis: 25.333%;
+  color: #fff;
+`;
+
+export default Award;
---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+// Components
+import Award from '../components/Award';
+
+// Styles
+import styled from 'styled-components';
+import { Hero, Content } from '../styles/styles';
+
+// Movie data
+import { MovieState } from '../components/moviesState';
+
+const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  console.log(url)
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const currentMovie = movies.filter(movie => movie.url === url);
+
+    setMovie(currentMovie[0]);
+  }, [movies, url]);
+
+  console.log(movie)
+
+  return (
+    (
+      movie &&
+      <Movie>
+        <MovieContent>
+          <h2 className="h3">Movie Detail</h2>
+          <img src={movie.mainImg} alt={movie.title}loading="lazy" />
+        </MovieContent>
+        <Awards>
+          {
+            movie.awards.map(award => (
+              <Award 
+                title={award.title}
+                description={award.description}
+                key={award.title}
+              />
+            ))
+          }
+        </Awards>
+        <ImgDisplay>
+          <img src={movie.secondaryImg} alt="" loading="lazy" />
+        </ImgDisplay>
+      </Movie>
+    )
+  );
+};
+
+const Movie  = styled(Hero)`
+  padding: 0 1.5rem;
+`;
+
+const MovieContent = styled(Content)`
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-around;
+  padding: 2rem 0;
+  h2 {
+    align-self: center;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+const Awards = styled.div`
+  min-height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 2rem 0;
+`;
+
+const ImgDisplay = styled.div`
+  min-height: 50vh;
+  padding: 2rem 0;
+  img {
+    min-height: 90vh;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+export default MovieDetail;
