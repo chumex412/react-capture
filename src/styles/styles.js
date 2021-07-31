1e0c2afa1d8d312ad77a6165845cf8aca8f46a57
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-07-31 02:34:52 +0100

    Refactored styles.js

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,13 +1,37 @@
 import styled from 'styled-components';
 
 export const Hero = styled.section`
-  padding: 2rem 0;
+  padding: 0 1.5rem;
   max-width: 90%;
   width: 100%;
   margin: 0 auto;
-  padding: 0 1.5rem;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 90vh;
+  color: #fff;
+`;
+
+export const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
 `;
 
 export const Hide = styled.div`
   overflow: hidden;
-`
+`;
+
+export const Description = styled.div`
+  flex: 1;
+  h2 {
+    span {
+      color: #23d997;
+    }
+  }
+  p {
+    padding: 2rem 0;
+  }
+`;
