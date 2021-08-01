d102cc4fa4c83a0cad0a59f9e0142e16c7a2027c
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-08-01 12:06:11 +0100

    Added and animated the wave svg

---
+++ src/components/Wave.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+
+const Wave = () => {
+
+  return (
+    <WaveSvg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 363">
+      <motion.path
+       initial={{ pathLength: 0, pathOffset: 1 }}
+       animate={{ pathLength: 1, pathOffset: 0 }}
+       transition={{ duration: 1 }}
+       d="M1440 27.5c-87.3-7.7-317.6 21.6-540.7 199.8-278.8 222.8-545 128.3-729-42C23.2 49.1-4.2 8.3.5 5" stroke="#D96ED4"
+       strokeWidth="10"
+       strokeOpacity="0.5"
+      />
+    </WaveSvg>
+
+  );
+};
+
+const WaveSvg = styled.svg`
+  position: absolute;
+  left: 0;
+  z-index: 1
+`;
+
+export default Wave;
